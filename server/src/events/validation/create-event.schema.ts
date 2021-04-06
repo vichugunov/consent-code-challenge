bc3664abf835650908455b0972aea102b24b85da
Victor Chugunov <vichugunov@gmail.com>
2021-04-06 10:34:09 +0200

    mark all fields as required

--- server/src/events/validation/create-event.schema.ts
+++ server/src/events/validation/create-event.schema.ts
@@ -1,12 +1,12 @@
 import * as Joi from 'joi'
 
 const UserIdentifierSchema = Joi.object({
-  id: Joi.string().guid()
+  id: Joi.string().guid().required()
 })
 
 const ConsentSchema = Joi.object({
-  id: Joi.string().valid('email_notifications', 'sms_notifications'),
-  enabled: Joi.boolean()
+  id: Joi.string().valid('email_notifications', 'sms_notifications').required(),
+  enabled: Joi.boolean().required()
 })
 
 export const createEventSchema = Joi.object({
--- server/src/users/validation/create-user.schema.ts
+++ server/src/users/validation/create-user.schema.ts
@@ -1,5 +1,5 @@
 import * as Joi from 'joi'
 
 export const createUserSchema = Joi.object({
-  email: Joi.string().email()
+  email: Joi.string().email().required()
 })
